020698d8598f24fbcefbb79ef3f6ba86e2804b1f
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-17 16:54:07 +0400

    Travis should be able to push to docker
    Signed-off-by: Yervand Aghababyan <yervand.aghababyan@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,13 +4,13 @@ docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 if [ $TRAVIS_BRANCH == 'develop' ]
 then
-    mvn -P snapshot -P docker-snapshot clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
+    mvn -P snapshot clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
 elif [ $TRAVIS_BRANCH == 'master' ]
 then
-    mvn -P release -P docker-release clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
+    mvn -P release clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY

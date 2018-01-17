eaa90723dd49bec49f4a82d2d51d454cb2300409
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-17 14:49:09 +0400

    Minor change to Travis build
    Signed-off-by: Yervand Aghababyan <yervand.aghababyan@gmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 
-if [ $TRAVIS_BRANCH == 'develop' ] || [ $TRAVIS_BRANCH == 'master' ]
+if [ $TRAVIS_BRANCH == 'develop' ] ||
 then
-    ls -al /
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
+    mvn -P docker-snapshot clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=sfl \
+    -Dsonar.login=$SONARCLOUD_KEY
+elif [ $TRAVIS_BRANCH == 'master' ]
+    mvn -P docker-release clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY

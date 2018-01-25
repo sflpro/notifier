3948578b62b25dc814406b2b267ebe7bd1004a00
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-25 12:21:28 +0400

    Sonar analysis

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,18 +2,30 @@
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-if [ $TRAVIS_BRANCH == 'develop' ]
+if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-    mvn -P snapshot clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
+    echo "Running develop branch build and analysis. Snapshots will be published. All issues/stats will be saved to Sonar database."
+    mvn -P snapshot -P central clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
-elif [ $TRAVIS_BRANCH == 'master' ]
+elif [ $TRAVIS_BRANCH == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-    mvn -P release clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
+    mvn -P release -P central clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.organization=sfl \
     -Dsonar.login=$SONARCLOUD_KEY
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    echo "Running Github PR build and analysis. Sonar will run in preview mode."
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=sfl \
+    -Dsonar.login=$SONARCLOUD_KEY \
+    -Dsonar.analysis.mode=preview \
+    -Dsonar.github.repository=sflpro/identity \
+    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+    -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH_TOKEN
 else
     mvn clean verify -B
 fi

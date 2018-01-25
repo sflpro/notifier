40286162f0d9fdf59b636462e9322ceaeff3a7e7
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2018-01-25 13:26:00 +0400

    Trying gpg encryption

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,8 +2,11 @@
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
+
+
 if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
+    init_gpg
     echo "Running develop branch build and analysis. Snapshots will be published. All issues/stats will be saved to Sonar database."
     mvn -P snapshot -P central clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
@@ -11,6 +14,7 @@ then
     -Dsonar.login=$SONARCLOUD_KEY
 elif [ $TRAVIS_BRANCH == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
+    init_gpg
     echo "Running develop branch build and analysis. Snapshots will be published.. Sonar run will be skipped."
     mvn -P release -P central clean org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
@@ -18,6 +22,7 @@ then
     -Dsonar.login=$SONARCLOUD_KEY
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
+    init_gpg
     echo "Running Github PR build and analysis. Sonar will run in preview mode."
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B \
     -Dsonar.host.url=https://sonarcloud.io \
@@ -31,3 +36,10 @@ else
     echo "Running regular maven execution. No artifacts will be released to either release or snapshot repositories"
     mvn clean verify -B
 fi
+
+init_gpg() {
+    echo "Decrypt encrypted gpg key"
+    openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_iv -in ./travis/travis-gpg-key.asc.enc -out ./travis/travis-gpg-key.asc -d
+    echo "Import gpg key"
+    gpg --import ./travis/travis-gpg-key.asc
+}

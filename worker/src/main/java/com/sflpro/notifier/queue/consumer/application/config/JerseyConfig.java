f4e38a8370c7300495ba1f637257a866299caa98
Davit Harutyunyan <h_davit@yahoo.com>
2018-01-28 17:14:26 +0400

    Jersey config chanegs

--- worker/src/main/java/com/sflpro/notifier/queue/consumer/application/config/JerseyConfig.java
+++ worker/src/main/java/com/sflpro/notifier/queue/consumer/application/config/JerseyConfig.java
@@ -1,8 +1,15 @@
 package com.sflpro.notifier.queue.consumer.application.config;
 
+
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.filter.RequestContextFilter;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Path;
+import java.util.Map;
 
 /**
  * Company: SFL LLC
@@ -12,9 +19,20 @@ import org.springframework.web.filter.RequestContextFilter;
  */
 public class JerseyConfig extends ResourceConfig {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     public JerseyConfig() {
         register(RequestContextFilter.class);
         packages("com.sflpro.notifier.queue.consumer.application");
         register(LoggingFilter.class);
+    }
+
+    @PostConstruct
+    public void setup() {
+        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(Path.class);
+        for (Object o : beans.values()) {
+            register(o);
+        }
     }
 }

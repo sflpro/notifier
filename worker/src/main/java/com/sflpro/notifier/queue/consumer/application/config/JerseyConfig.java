8053ef3d27466d2bff164f5eafefc1877b7bc7e4
Davit Harutyunyan <h_davit@yahoo.com>
2018-01-28 17:48:07 +0400

    Add consumer classes to jar info

--- worker/src/main/java/com/sflpro/notifier/queue/consumer/application/config/JerseyConfig.java
+++ worker/src/main/java/com/sflpro/notifier/queue/consumer/application/config/JerseyConfig.java
@@ -3,13 +3,7 @@ package com.sflpro.notifier.queue.consumer.application.config;
 
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.filter.RequestContextFilter;
-
-import javax.annotation.PostConstruct;
-import javax.ws.rs.Path;
-import java.util.Map;
 
 /**
  * Company: SFL LLC
@@ -19,20 +13,9 @@ import java.util.Map;
  */
 public class JerseyConfig extends ResourceConfig {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     public JerseyConfig() {
         register(RequestContextFilter.class);
-        packages("com.sflpro.notifier.queue.consumer.application");
+        packages("com.sflpro.notifier.queue.consumer.notification");
         register(LoggingFilter.class);
-    }
-
-    @PostConstruct
-    public void setup() {
-        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(Path.class);
-        for (Object o : beans.values()) {
-            register(o);
-        }
     }
 }

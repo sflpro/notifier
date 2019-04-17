5f65ff73f64feca6c25539d154f3c4dcebcf10e7
Ruben Vardanyan <ruben.vardanyan@sflpro.com>
2019-04-17 22:16:33 +0400

    Added templating service and freemarker

---
+++ core/core-services-impl/src/main/java/com/sflpro/notifier/services/template/TemplatingServiceImpl.java
@@ -0,0 +1,52 @@
+package com.sflpro.notifier.services.template;
+
+import com.sflpro.notifier.services.common.exception.ServicesRuntimeException;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+/**
+ * User: Ruben Vardanyan
+ * Company: SFL LLC
+ * Date: 4/17/19
+ * Time: 9:36 PM
+ */
+@Component
+public class TemplatingServiceImpl implements TemplatingService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TemplatingServiceImpl.class);
+
+    @Autowired
+    private Configuration freemarkerConfiguration;
+
+    public TemplatingServiceImpl() {
+        LOGGER.debug("Initializing templating service");
+    }
+
+    @Override
+    public String getContentForTemplate(@Nonnull final String templateName, @Nonnull final Map<String, String> parameters) {
+        Assert.notNull(templateName, "Template name should not be null");
+        Assert.notNull(parameters, "Parameters should not be null");
+        try {
+            final Template template = freemarkerConfiguration.getTemplate(templateName);
+            final StringWriter resultWriter = new StringWriter();
+            template.process(parameters, resultWriter);
+            final String result = resultWriter.toString();
+            resultWriter.flush();
+            resultWriter.close();
+            return result;
+        } catch (IOException | TemplateException e) {
+            LOGGER.error("Error occurred while processing template for template name - {}, with parameters -{}", templateName, parameters);
+            throw new ServicesRuntimeException("Error occurred while processing template for template name - " + templateName + " with parameters - " + parameters, e);
+        }
+    }
+}

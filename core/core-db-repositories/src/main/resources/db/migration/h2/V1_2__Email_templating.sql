a07041c86b480825308dc79b3157d5a5fd2a8530
Ruben Vardanyan <rub.vardanyan@gmail.com>
2019-04-22 21:36:57 +0400

    Fixed h2 migration script

--- core/core-db-repositories/src/main/resources/db/migration/h2/V1_2__Email_templating.sql
+++ core/core-db-repositories/src/main/resources/db/migration/h2/V1_2__Email_templating.sql
@@ -1,8 +1,8 @@
 ALTER TABLE notification_email_third_party_property
     RENAME TO notification_email_property;
-ALTER TABLE notification_email_third_party_property
+ALTER TABLE notification_email_property
     DROP CONSTRAINT fk_notif_third_party_property_notif_third_party;
-ALTER TABLE notification_email_third_party_property
+ALTER TABLE notification_email_property
     ADD CONSTRAINT fk_notif_email_property_notif_email
         FOREIGN KEY (notification_id)
             REFERENCES notification_email (id)

8ff8d3fbdfd1b6b08d5fd9c0c6ff77a79ae4e3ae
syuzanna.eprikyan <syuzanna.eprikyan@sflpro.com>
2019-04-29 19:00:06 +0400

    #16 fixed MySQl queries

--- core/core-db-repositories/src/main/resources/db/migration/mysql/V1_2__Email_templating.sql
+++ core/core-db-repositories/src/main/resources/db/migration/mysql/V1_2__Email_templating.sql
@@ -1,7 +1,9 @@
 ALTER TABLE notification_email_third_party_property
     RENAME notification_email_property;
+
 ALTER TABLE notification_email_property
     DROP FOREIGN KEY fk_notif_third_party_property_notif_third_party;
+
 ALTER TABLE notification_email_property
     ADD CONSTRAINT fk_notif_email_property_notif_email
         FOREIGN KEY (notification_id)

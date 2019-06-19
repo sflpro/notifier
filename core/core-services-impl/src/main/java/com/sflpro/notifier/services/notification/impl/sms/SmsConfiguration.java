9853d461f2d9eaaf2b87a96eca72d2f47106595a
Hayk Mkrtchyan <hayk.mkrtchyan@sflpro.com>
2019-06-19 16:17:39 +0400

    Removing assertion when no sms sender was registered.

--- core/core-services-impl/src/main/java/com/sflpro/notifier/services/notification/impl/sms/SmsConfiguration.java
+++ core/core-services-impl/src/main/java/com/sflpro/notifier/services/notification/impl/sms/SmsConfiguration.java
@@ -1,10 +1,11 @@
 package com.sflpro.notifier.services.notification.impl.sms;
 
 import com.sflpro.notifier.sms.SmsSenderRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -17,9 +18,13 @@ import java.util.List;
 @ComponentScan("com.sflpro.notifier.externalclients.sms.registry")
 class SmsConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SmsConfiguration.class);
+
     @Bean
     SmsSenderProvider smsSenderProvider(final List<SmsSenderRegistry> registries) {
-        Assert.notEmpty(registries, "No any component was registered for sending sms!");
+        if(registries.isEmpty()){
+            LOGGER.info("No any component was registered for sending sms!");
+        }
         return new DefaultSmsSenderProvider(registries);
     }
 }

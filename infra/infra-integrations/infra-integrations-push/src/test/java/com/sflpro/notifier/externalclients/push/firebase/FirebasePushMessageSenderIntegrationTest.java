80a6fb995d6fbf40c216cb64671cc8e1973e7c94
Davit Harutyunyan <h_davit@yahoo.com>
2020-01-21 18:17:32 +0400

    Firebase integration test marked as ignored. TODO remove all sensitive data(tokens, private keys) from project.

--- infra/infra-integrations/infra-integrations-push/src/test/java/com/sflpro/notifier/externalclients/push/firebase/FirebasePushMessageSenderIntegrationTest.java
+++ infra/infra-integrations/infra-integrations-push/src/test/java/com/sflpro/notifier/externalclients/push/firebase/FirebasePushMessageSenderIntegrationTest.java
@@ -29,7 +29,7 @@ public class FirebasePushMessageSenderIntegrationTest extends AbstractPushNotifi
     @Test
     public void testSend(){
         assertThat(pushMessageSender.send(PushMessage.of(
-                "AAAAaq6D4Jg:APA91bEuyitADDXu9tm7NtyIoSU3PmjFDC0F5rzRWl2afEkkv6aBBmt60NBAAN5YrVFA2i18MdLdtBpXF5Zr-JNS4CrlPnfB3JXn4_UPjRh6dPuX92LoK8f0hvCaeKSkBRcyjt1DUKtb", // TODO remove secure variables and private keys from project
+                "cYHsFopdjms:APA91bGv-70ceTYWQ0HqguGMW3U25z1LTE5ArdhkQfBk88Ww6i2SBKWSB_mX0T-mNVRu8fu3pChQZRGKkxb_ilOQ4nGclcSn3xOqBD_6AqWah2KVxGfpciZ0x_tFjy6IaoZOBlGwvx9z", // TODO remove secure variables and private keys from project
                 "Hey!",
                 "Hey amigo!",
                 PlatformType.GCM,

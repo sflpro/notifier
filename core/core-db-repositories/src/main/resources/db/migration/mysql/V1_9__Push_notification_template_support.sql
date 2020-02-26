359274828f3223634dbf23845927cdc3e7242120
vazgen.danielyan <vazgen.danielyan@sflpro.com>
2020-02-27 01:17:14 +0400

    Fixed mysql related migration script

--- core/core-db-repositories/src/main/resources/db/migration/mysql/V1_9__Push_notification_template_support.sql
+++ core/core-db-repositories/src/main/resources/db/migration/mysql/V1_9__Push_notification_template_support.sql
@@ -4,8 +4,6 @@ alter table notification_push
 alter table notification_push
     add column locale varchar(5) default null;
 
-alter table notification_email
-    alter column locale type varchar(5) using locale::varchar(5);
+alter table notification_email MODIFY column locale varchar(5);
 
-alter table notification_sms
-    alter column locale type varchar(5) using locale::varchar(5);
+alter table notification_sms MODIFY column locale varchar(5);

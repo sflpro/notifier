7c1d34a77efd072fb3718d5d7c6451dff8bc60a9
vazgen.danielyan <vazgen.danielyan@sflpro.com>
2020-02-27 00:50:26 +0400

    Fixed migration script.

--- core/core-db-repositories/src/main/resources/db/migration/postgresql/V1_9__Push_notification_template_support.sql
+++ core/core-db-repositories/src/main/resources/db/migration/postgresql/V1_9__Push_notification_template_support.sql
@@ -2,4 +2,10 @@ alter table notification_push
     add column template_name varchar(255) default null;
 
 alter table notification_push
-    add column locale char(2) default null;
+    add column locale varchar(5) default null;
+
+alter table notification_email
+    alter column locale type varchar(5) using locale::varchar(5);
+
+alter table notification_sms
+    alter column locale type varchar(5) using locale::varchar(5);

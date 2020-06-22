13c372122dc4c3fef5e34d72b2487db0955eb853
davit.harutyunyan <h_davit@yahoo.com>
2020-06-22 11:06:18 +0400

    Fix migration scripts

--- core/core-db-repositories/src/main/resources/db/migration/mysql/V1_0_0__Base_schema.sql
+++ core/core-db-repositories/src/main/resources/db/migration/mysql/V1_0_0__Base_schema.sql
@@ -42,18 +42,6 @@ CREATE TABLE IF NOT EXISTS notification_email (
   CONSTRAINT fk_notification_email_notification FOREIGN KEY (id) REFERENCES notification (id)
 );
 
-CREATE TABLE IF NOT EXISTS notification_email_file_attachment (
-  id BIGINT not null AUTO_INCREMENT PRIMARY KEY,
-  file_name VARCHAR(255) NOT NULL,
-  url VARCHAR(1000) NOT NULL,
-  mime_type VARCHAR(255),
-  notification_email_id BIGINT NOT NULL,
-  CONSTRAINT fk_notification_email_file_attachment_notification_email FOREIGN KEY (notification_email_id) REFERENCES notification_email (id),
-  created DATETIME NOT NULL,
-  removed DATETIME,
-  updated DATETIME NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS notification_email_third_party (
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   CONSTRAINT fk_notification_email_third_party_notification_email FOREIGN KEY (id) references notification_email (id)
--- core/core-db-repositories/src/main/resources/db/migration/postgresql/V1_0_0__Base_schema.sql
+++ core/core-db-repositories/src/main/resources/db/migration/postgresql/V1_0_0__Base_schema.sql
@@ -44,17 +44,6 @@ create table if not exists notification_email (
   template_name varchar(255)
 );
 
-create table if not exists notification_email_file_attachment (
-  id bigint not null constraint pk_notification_email_file_attachment primary key,
-  file_name varchar(255) not null,
-  url varchar (1000) not null,
-  mime_type varchar(255),
-  notification_email_id bigint constraint fk_notification_email_file_attachment_notification_email references notification_email,
-  created timestamp not null,
-  removed timestamp,
-  updated timestamp not null
-);
-
 create table if not exists notification_email_third_party (
   id bigint not null constraint pk_notification_email_third_party
     primary key constraint fk_notification_email_third_party_notification_email references notification_email

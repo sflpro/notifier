6fc1f25e283dff218c4dc9786c106fa6f28c9315
Norik Aslanayan <norik.aslanyan@bynder.com>
2020-11-17 16:36:03 +0400

    Added additiional facade layer for notification geting for fixed hibernate lazy exception

--- api/api-facade/src/test/java/com/sflpro/notifier/api/facade/services/NotificationFacadeImplTest.java
+++ api/api-facade/src/test/java/com/sflpro/notifier/api/facade/services/NotificationFacadeImplTest.java
@@ -1,9 +1,8 @@
-package com.sflpro.notifier.api.facade.endpoints.notification;
+package com.sflpro.notifier.api.facade.services;
 
-import com.sflpro.notifier.api.facade.services.NotificationFacade;
-import com.sflpro.notifier.api.model.email.EmailNotificationModel;
 import com.sflpro.notifier.api.model.notification.NotificationModel;
 import com.sflpro.notifier.db.entities.notification.email.EmailNotification;
+import com.sflpro.notifier.services.notification.NotificationService;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,22 +13,23 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class NotificationResourceTest {
+public class NotificationFacadeImplTest {
 
     @Mock
-    private NotificationFacade notificationFacade;
+    private NotificationService notificationService;
 
     @InjectMocks
-    private NotificationResource notificationResource;
+    private NotificationFacadeImpl notificationFacade;
 
     @Test
     public void get() {
         final Long id = RandomUtils.nextLong(1, 100);
-        final EmailNotificationModel emailNotification = new EmailNotificationModel();
+        final EmailNotification emailNotification = new EmailNotification();
         emailNotification.setId(id);
-        Mockito.when(notificationFacade.get(id)).thenReturn(emailNotification);
+        Mockito.when(notificationService.getNotificationById(id)).thenReturn(emailNotification);
 
-        final NotificationModel notificationModel = notificationResource.get(id);
+        final NotificationModel notificationModel = notificationFacade.get(id);
         Assert.assertEquals(id, notificationModel.getId());
     }
+
 }
